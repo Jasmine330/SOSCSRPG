819421e9842ee45fc272489d7e6c7b99159208d2
Jasmina <jasminethapa330@gmail.com>
2025-03-26 15:30:37 +0545

    Player Class

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -8,7 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Engine.ViewModels;
 namespace WPFUI;
 
 /// <summary>
@@ -16,8 +16,13 @@ namespace WPFUI;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private GameSession _gameSession;
     public MainWindow()
     {
         InitializeComponent();
+
+        _gameSession = new GameSession();
+
+        DataContext = _gameSession;
     }
 }
