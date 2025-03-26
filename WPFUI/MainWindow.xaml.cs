198f57479e112e07f5bfc9bea6b35b84458838d1
Jasmina <jasminethapa330@gmail.com>
2025-03-26 16:28:40 +0545

    //Connecting Changes to the UI

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -20,9 +20,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-
         _gameSession = new GameSession();
-
         DataContext = _gameSession;
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        _gameSession.CurrentPlayer.ExperiencePoints = _gameSession.CurrentPlayer.ExperiencePoints + 10;
     }
 }
