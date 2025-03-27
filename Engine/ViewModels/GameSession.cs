ac3feb82c79d9675b3453741b3e6736a6aa3486c
Jasmina <jasminethapa330@gmail.com>
2025-03-27 11:58:34 +0545

    creating world

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Engine.Factories;
 using Engine.Models;
 
 namespace Engine.ViewModels
 {
     public class GameSession
     {
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-
-        public GameSession() 
+        public Location CurrentLocation { get; set; }
+        public GameSession()
         {
             CurrentPlayer = new Player();
-            CurrentPlayer.Name = "Jasmine";
+            CurrentPlayer.Name = "Scott";
             CurrentPlayer.CharacterClass = "Fighter";
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "/Engine;component/Images/Locations/Home.png";
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory;
         }
     }
 }
