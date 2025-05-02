ec6c89581abbdaa9ab150bdd8d886bf246b68f6a
Jasmina <jasminethapa330@gmail.com>
2025-05-02 07:56:56 +0545

    Monster Combat

--- Engine/Factories/ItemFactory.cs
+++ Engine/Factories/ItemFactory.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Engine.Models;
-
 namespace Engine.Factories
 {
     public static class ItemFactory
     {
         private static List<GameItem> _standardGameItems;
-
-        //static class do not have contructor because they are never instantiated but yet we can use it as
-        //this function will first executed when the class is loaded
         static ItemFactory()
         {
             _standardGameItems = new List<GameItem>();
-
             _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
             _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
             _standardGameItems.Add(new GameItem(9001, "Snake fang", 1));
@@ -26,16 +18,17 @@ namespace Engine.Factories
             _standardGameItems.Add(new GameItem(9005, "Spider fang", 1));
             _standardGameItems.Add(new GameItem(9006, "Spider silk", 2));
         }
-
         public static GameItem CreateGameItem(int itemTypeID)
         {
             GameItem standardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
-
-            if(standardItem != null)
+            if (standardItem != null)
             {
+                if (standardItem is Weapon)
+                {
+                    return (standardItem as Weapon).Clone();
+                }
                 return standardItem.Clone();
             }
-
             return null;
         }
     }
--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Engine.EventArgs;
 using Engine.Factories;
 using Engine.Models;
@@ -7,14 +8,11 @@ namespace Engine.ViewModels
     public class GameSession : BaseNotificationClass
     {
         public event EventHandler<GameMessageEventArgs> OnMessageRaised;
-
         #region Properties
-
         private Location _currentLocation;
         private Monster _currentMonster;
         public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
-
         public Location CurrentLocation
         {
             get { return _currentLocation; }
@@ -26,34 +24,26 @@ namespace Engine.ViewModels
                 OnPropertyChanged(nameof(HasLocationToEast));
                 OnPropertyChanged(nameof(HasLocationToWest));
                 OnPropertyChanged(nameof(HasLocationToSouth));
-
                 GivePlayerQuestsAtLocation();
                 GetMonsterAtLocation();
-
-
             }
         }
-
         public Monster CurrentMonster
         {
             get { return _currentMonster; }
             set
             {
                 _currentMonster = value;
-
                 OnPropertyChanged(nameof(CurrentMonster));
                 OnPropertyChanged(nameof(HasMonster));
-
-                if(CurrentMonster != null)
+                if (CurrentMonster != null)
                 {
                     RaiseMessage("");
-                    RaiseMessage($"You see a {CurrentMonster.Name} here.");
+                    RaiseMessage($"You see a {CurrentMonster.Name} here!");
                 }
             }
         }
-
-        #endregion
-
+        public Weapon CurrentWeapon { get; set; }
         public bool HasLocationToNorth
         {
             get
@@ -82,8 +72,8 @@ namespace Engine.ViewModels
                 return CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate) != null;
             }
         }
-
         public bool HasMonster => CurrentMonster != null;
+        #endregion
         public GameSession()
         {
             CurrentPlayer = new Player
@@ -95,6 +85,10 @@ namespace Engine.ViewModels
                 ExperiencePoints = 0,
                 Level = 1
             };
+            if (!CurrentPlayer.Weapons.Any())
+            {
+                CurrentPlayer.AddItemToInventory(ItemFactory.CreateGameItem(1001));
+            }
             CurrentWorld = WorldFactory.CreateWorld();
             CurrentLocation = CurrentWorld.LocationAt(0, 0);
         }
@@ -126,7 +120,6 @@ namespace Engine.ViewModels
                 CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
             }
         }
-
         private void GivePlayerQuestsAtLocation()
         {
             foreach (Quest quest in CurrentLocation.QuestsAvailableHere)
@@ -137,12 +130,69 @@ namespace Engine.ViewModels
                 }
             }
         }
-
         private void GetMonsterAtLocation()
         {
             CurrentMonster = CurrentLocation.GetMonster();
         }
-
+        public void AttackCurrentMonster()
+        {
+            if (CurrentWeapon == null)
+            {
+                RaiseMessage("You must select a weapon, to attack.");
+                return;
+            }
+            // Determine damage to monster
+            int damageToMonster = RandomNumberGenerator.NumberBetween(CurrentWeapon.MinimumDamage, CurrentWeapon.MaximumDamage);
+            if (damageToMonster == 0)
+            {
+                RaiseMessage($"You missed the {CurrentMonster.Name}.");
+            }
+            else
+            {
+                CurrentMonster.HitPoints -= damageToMonster;
+                RaiseMessage($"You hit the {CurrentMonster.Name} for {damageToMonster} points.");
+            }
+            // If monster if killed, collect rewards and loot
+            if (CurrentMonster.HitPoints <= 0)
+            {
+                RaiseMessage("");
+                RaiseMessage($"You defeated the {CurrentMonster.Name}!");
+                CurrentPlayer.ExperiencePoints += CurrentMonster.RewardExperiencePoints;
+                RaiseMessage($"You receive {CurrentMonster.RewardExperiencePoints} experience points.");
+                CurrentPlayer.Gold += CurrentMonster.RewardGold;
+                RaiseMessage($"You receive {CurrentMonster.RewardGold} gold.");
+                foreach (ItemQuantity itemQuantity in CurrentMonster.Inventory)
+                {
+                    GameItem item = ItemFactory.CreateGameItem(itemQuantity.ItemID);
+                    CurrentPlayer.AddItemToInventory(item);
+                    RaiseMessage($"You receive {itemQuantity.Quantity} {item.Name}.");
+                }
+                // Get another monster to fight
+                GetMonsterAtLocation();
+            }
+            else
+            {
+                // If monster is still alive, let the monster attack
+                int damageToPlayer = RandomNumberGenerator.NumberBetween(CurrentMonster.MinimumDamage, CurrentMonster.MaximumDamage);
+                if (damageToPlayer == 0)
+                {
+                    RaiseMessage("The monster attacks, but misses you.");
+                }
+                else
+                {
+                    CurrentPlayer.HitPoints -= damageToPlayer;
+                    RaiseMessage($"The {CurrentMonster.Name} hit you for {damageToPlayer} points.");
+                }
+                // If player is killed, move them back to their home.
+                if (CurrentPlayer.HitPoints <= 0)
+                {
+                    RaiseMessage("");
+                    RaiseMessage($"The {CurrentMonster.Name} killed you.");
+                    CurrentLocation = CurrentWorld.LocationAt(0, -1); // Player's home
+                    CurrentPlayer.HitPoints = CurrentPlayer.Level * 10; // Completely heal the player
+                }
+            }
+        }
         private void RaiseMessage(string message)
         {
             OnMessageRaised?.Invoke(this, new GameMessageEventArgs(message));
