ee4639d982639bb510682a7c5629c0b783dcc9ee
Jasmina <jasminethapa330@gmail.com>
2025-05-03 21:53:11 +0545

    Completing quests

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -9,12 +9,13 @@ namespace WPFUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private GameSession _gameSession;
+        private readonly GameSession _gameSession = new GameSession();
         public MainWindow()
         {
             InitializeComponent();
-            _gameSession = new GameSession();
+            
             _gameSession.OnMessageRaised += OnGameMessageRaised;
+
             DataContext = _gameSession;
         }
         private void OnClick_MoveNorth(object sender, RoutedEventArgs e)
